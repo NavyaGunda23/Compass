9f5c56eb74797b7c964826154883b87194e11a49
NavyaGunda23 <52776081+NavyaGunda23@users.noreply.github.com>
2021-10-20 18:52:25 +0530

    rpelacing files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Sidebar from './components/Sidebar'
+import BarGraph from './components/BarGraph'
+import PieCharts from './components/PieCharts'
+import Legends from './components/Legends'
+import './App.css'
+
 
 function App() {
+
+  
+
+  const [ data, setData] = useState({
+    piecartLegends:[
+      {
+      name:"Auto",
+      color:"#FE863D "
+      },
+      {
+        name:"Motorcycle",
+        color:"#01AA9F"
+      },
+      {
+        name:"RV",
+        color:"#FEB646"
+      },
+      {
+        name:"Water Craft",
+        color:"#015FA7"
+      },
+      {
+        name:"N/A",
+        color:"#28813E"
+      }
+    ],
+    barLegends:[
+      {
+        name:"Count Case ID",
+        color:"#003e75"
+      }
+    ]
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar />
+      <div className="right-content">
+        <h4>Insight Page</h4>
+        <div className="graph-parent">
+            <div className="graph-section">
+              <h4>Vehicle Type</h4>
+              <div className="graphs-data">
+                <PieCharts />
+                 <div>
+                 <Legends leg_data={data.piecartLegends}/>
+                 </div>
+              </div>
+              <p className="x_axis_label">Vehicle Type</p>
+            </div>
+            <div className="graph-section">
+               <h4>Home Mortage by work status</h4>
+               <p className="y_axis_label">Count Case ID</p>
+               <div className="graphs-data">
+                  <BarGraph  />
+                  <div>
+                    <Legends leg_data={data.barLegends}/>
+                  </div>
+                </div>
+                <p className="x_axis_label">Work Status</p>
+            </div>
+        </div>
+         
+          
+      </div>
+      
     </div>
   );
 }
